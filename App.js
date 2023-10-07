d10bef4eb5e6545172a58d8a7552ab297cc13a32
LanceCamops5302 <54639300+LanceCampos5302@users.noreply.github.com>
2023-10-07 00:43:10 -0400

    SignUp started

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginPage from './components/LoginPage';
+import SignUpPage from './components/SignUpPage';
 
 export default function App() {
   return (
---
+++ components/SignUpPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { globalStyles, resetStyles } from '../AppStyles'
+
+const SignUpPage = () => {
+  return (
+    <View style={[resetStyles.resetStyles]}>
+        <Text>SignUpPage</Text>
+    </View>
+  );
+};
+
+export default SignUpPage;
