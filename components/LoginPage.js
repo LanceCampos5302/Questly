ebaa3012a1e1285c736f1df3d77fa8ef5fa0c18e
Danyal Siddiqi <dantsiddiqi@gmail.com>
2023-10-07 01:16:18 -0400

    created login page

--- components/LoginPage.js
+++ components/LoginPage.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { globalStyles, resetStyles } from '../AppStyles'
 
-
-
-
 const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Here you can call your API to log in the user with username and password
+    console.log(username, password);
+  }
+
   return (
-    <View style={[resetStyles.resetStyles]}>
-        <Text style={[styles.text]}>Questly</Text>
+    <View style={[styles.container]}>
+      <Text style={[styles.text]}>Questly</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        onChangeText={setUsername}
+        value={username}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        onChangeText={setPassword}
+        value={password}
+        secureTextEntry
+      />
+
+      <Button title="Login" onPress={handleLogin} />
     </View>
   );
 };
@@ -20,14 +42,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    padding: 16,
   },
   text: {
     fontSize: 30,
     fontWeight: 'bold',
     color: globalStyles.primaryColor,
+    marginBottom: 16,
   },
-  paper:{
-    fontSize: 16,
-    fontWeight: 'bold',
-  }
+  input: {
+    height: 40,
+    width: '100%',
+    marginBottom: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 });
