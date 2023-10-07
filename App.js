4d537bed8a0582abeeceaa069437e6781565654c
LanceCamops5302 <54639300+LanceCampos5302@users.noreply.github.com>
2023-10-07 16:26:45 -0400

    revert

--- App.js
+++ App.js
@@ -1,26 +1,25 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 import LoginPage from './components/LoginPage';
 import SignUpPage from './components/SignUpPage';
 import MenuPage from './components/MenuPage';
 import QuestPage from './components/QuestPage';
 import ProfilePage from './components/ProfilePage';
 
-const Stack = createStackNavigator();
-
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="QuestPage">
-        <Stack.Screen name="LoginPage" component={LoginPage} />
-        <Stack.Screen name="SignUpPage" component={SignUpPage} />
-        <Stack.Screen name="MenuPage" component={MenuPage} />
-        <Stack.Screen name="QuestPage" component={QuestPage} />
-        <Stack.Screen name="ProfilePage" component={ProfilePage} /> {/* Add this line */}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+        <QuestPage></QuestPage>
+        <StatusBar style="auto" />
+    </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
