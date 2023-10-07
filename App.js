28d001176be8ecc4867afee9dba602c152781ce3
LanceCamops5302 <54639300+LanceCampos5302@users.noreply.github.com>
2023-10-06 23:37:42 -0400

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>On github</Text>
+      <Text>Questly/Questly</Text>
       <StatusBar style="auto" />
     </View>
   );
