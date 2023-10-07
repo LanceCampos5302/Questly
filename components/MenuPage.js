17ebffe8822269ec74c16498f3ddf3ff7429731e
LanceCamops5302 <54639300+LanceCampos5302@users.noreply.github.com>
2023-10-07 01:16:18 -0400

    Worked on MenuNav

---
+++ components/MenuPage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { globalStyles, resetStyles } from '../AppStyles'
+import MenuNav from './MenuNav';
+
+const MenuPage = () => {
+  return (
+    <View style={[resetStyles.resetStyles, styles.background]}>
+        <Text style={[styles.text]}>Menu</Text>
+        <MenuNav></MenuNav>
+    </View>
+  );
+};
+
+export default MenuPage;
+
+const styles = StyleSheet.create({
+    background: {
+      width: '100%',
+      height: '100%',
+      backgroundColor: globalStyles.secondaryColor,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    text: {
+      fontSize: 30,
+      fontWeight: 'bold',
+      color: globalStyles.primaryColor,
+    },
+    paper:{
+      fontSize: 16,
+      fontWeight: 'bold',
+    }
+  });
